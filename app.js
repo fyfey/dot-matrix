c32d0d7e81003400211cfc04177f1b4ce8286481
Stuart Fyfe <stuart@enableit.org.uk>
2015-06-18 21:20:25 +0100

    LocalStorage support and basic edit functionality

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 
-Backbone.sync = function(method, model) {
-    console.log("method", method, "model", model);
+// Backbone.sync = function(method, model) {
+//     console.log("method", method, "model", model);
+// 
+//     model.id = 1;
+// };
 
-    model.id = 1;
-};
+Font = Backbone.Model.extend({
 
-Font = Backbone.Model.extend({});
+    width: 5,
+    height: 7,
+
+    url: "fonts",
+
+    initialize: function() {
+        var data = _.range(this.height);
+        _.each(data, _.bind(function(i) {
+            data[i] = [];
+            for(j = 0; j < this.width; j++) {
+                data[i][j] = {set: false};
+            }
+        }, this));
+
+        this.set('data', data);
+
+        console.log(this);
+    },
+
+    toggleCell: function(x, y) {
+        var data = this.attributes.data;
+
+        if (data[y][x].set) {
+            data[y][x].set = false;
+        } else {
+            data[y][x].set = true;
+        }
+
+        this.set('data', data);
+        this.trigger('change:data', this, data);
+    },
+});
 Fonts = Backbone.Collection.extend({
+    localStorage: new Backbone.LocalStorage("Fonts"),
     model: Font
 });
 
@@ -17,18 +51,71 @@ HomeView = Backbone.View.extend({
     template: Handlebars.templates['home.hbs'],
 
     render: function() {
-        console.log("render");
         this.$el.html(this.template());
     }
 });
 
 FontEditView = Backbone.View.extend({
 
+    el: '.app',
+
+    events: {
+        'click .cell': 'handleClick',
+        'change input[data-field]': 'handleChange',
+        'click button[data-action=save]': 'handleSave'
+    },
+
     template: Handlebars.templates['font-edit.hbs'],
 
-    initialize: function() {
-        console.log("font edit!");
+    initialize: function(opts) {
+        this.model = opts.model;
+        console.log("model", this.model);
+        this.listenTo(this.model, 'change:data', this.render);
+    },
+
+    handleChange: function(e) {
+        console.log("handleChange");
+        var $el = $(e.currentTarget),
+            field = $el.data('field');
+
+        console.log("el", $el);
+
+        this.model.set(field, $el.val());
+    },
+
+    handleClick: function(e) {
+        var $el = $(e.currentTarget),
+            x = $el.data('x'),
+            y = $el.data('y');
+
+        this.model.toggleCell(x, y);
+    },
+
+    handleSave: function() {
+        this.model.save();
+    },
+
+    render: function() {
+        console.log("render", this.model.toJSON());
+        this.$el.html(this.template(this.model.toJSON()));
     }
+
+});
+
+FontListView = Backbone.View.extend({
+
+    el: '.app',
+
+    template: Handlebars.templates['font-list.hbs'],
+
+    initialize: function(opts) {
+        this.collection = opts.collection;
+    },
+
+    render: function() {
+        this.$el.html(this.template(this.collection.toJSON()));
+    }
+
 });
 
 Router = Backbone.Router.extend({
@@ -48,15 +135,38 @@ Router = Backbone.Router.extend({
 
     initialize: function() {
         this.navigate('home');
+        this.fonts = new Fonts();
     },
 
     home: function() {
-        view = new HomeView();
+        var view = new HomeView();
         view.render();
     },
 
-    fontEdit: function() {
+    fonts: function() {
+        this.fonts.fetch();
 
+        var view = new FontListView({collection: this.fonts});
+        view.render();
+    },
+
+    newFont: function() {
+        var font = new Font();
+        this.fonts.add(font);
+        var view = new FontEditView({model: font});
+        view.render();
+    },
+
+    editFont: function(id) {
+        console.log("editFont", id);
+        this.fonts
+            .fetch()
+            .then(_.bind(function() {
+                var font = this.fonts.get(id);
+                font.fetch();
+                var view = new FontEditView({model: font});
+                view.render();
+            }, this));
     }
 
 });
