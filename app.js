e594de7d4e9864b7592017d856edc29506394ced
Stuart Fyfe <stuart@stuartfyfe.uk>
2015-06-19 00:53:59 +0100

    Fixed undelegated events & added drag
    Views weren't being removed and therefore events were lingering around.
    Fixed by removing view on new view load.
    
    You can now drag to draw on the font edit page. You can only edit a cell
    once per drag

--- app.js
+++ app.js
@@ -1,176 +1,221 @@
 
-// Backbone.sync = function(method, model) {
-//     console.log("method", method, "model", model);
-// 
-//     model.id = 1;
-// };
+$(function() {
+    var Font = Backbone.Model.extend({
 
-Font = Backbone.Model.extend({
+        width: 5,
+        height: 7,
 
-    width: 5,
-    height: 7,
+        collection: fonts,
 
-    url: "fonts",
+        defaults: function() {
+            var data = _.range(this.height);
+            _.each(data, _.bind(function(i) {
+                data[i] = [];
+                for(j = 0; j < this.width; j++) {
+                    data[i][j] = {set: false};
+                }
+            }, this));
 
-    initialize: function() {
-        var data = _.range(this.height);
-        _.each(data, _.bind(function(i) {
-            data[i] = [];
-            for(j = 0; j < this.width; j++) {
-                data[i][j] = {set: false};
+            return {
+                data: data
             }
-        }, this));
+        },
 
-        this.set('data', data);
+        toggleCell: function(x, y) {
+            var data = this.attributes.data;
 
-        console.log(this);
-    },
+            data[y][x].set = !data[y][x].set;
 
-    toggleCell: function(x, y) {
-        var data = this.attributes.data;
+            this.set('data', data);
+            this.trigger('change:data', this, data);
+            this.trigger('change', this);
+        },
+    });
 
-        if (data[y][x].set) {
-            data[y][x].set = false;
-        } else {
-            data[y][x].set = true;
+    var FontCollection = Backbone.Collection.extend({
+        localStorage: new Backbone.LocalStorage("fonts"),
+        model: Font
+    });
+
+    var HomeView = Backbone.View.extend({
+
+        className: 'home-view',
+
+        template: Handlebars.templates['home.hbs'],
+
+        initialize: function(opts) {
+            $('.app').html(this.el);
+            this.render();
+        },
+
+        render: function() {
+            this.$el.html(this.template());
+            return this;
+        }
+    });
+
+    var FontEditView = Backbone.View.extend({
+
+        className: '.font-edit',
+
+        dragging: false,
+        currentDrag: [],
+
+        events: {
+            'mousedown .cell': 'handleClick',
+            'change input[data-field]': 'handleChange',
+            'click button[data-action=save]': 'handleSave',
+            'mousedown': 'startDrag',
+            'mouseup': 'endDrag',
+            'mouseover .cell': 'handleHover',
+        },
+
+        template: Handlebars.templates['font-edit.hbs'],
+
+        initialize: function(opts) {
+            this.model = opts.model;
+            this.listenTo(this.model, 'change:data', this.render);
+
+            $('body').html(this.el);
+            this.render();
+        },
+
+        handleChange: function(e) {
+            var $el = $(e.currentTarget),
+                field = $el.data('field');
+
+            this.model.set(field, $el.val());
+        },
+
+        handleClick: function(e) {
+            e.preventDefault();
+            var $el = $(e.currentTarget),
+                x = $el.data('x'),
+                y = $el.data('y');
+
+            this.model.toggleCell(x, y);
+            this.currentDrag.push(x+':'+y);
+            this.startDrag(e);
+        },
+
+        handleHover: function(e) {
+            var $el = $(e.currentTarget),
+                x = $el.data('x'),
+                y = $el.data('y');
+
+            if (this.dragging && !_.contains(this.currentDrag, x+':'+y)) {
+                this.currentDrag.push(x+':'+y);
+                this.model.toggleCell(x, y);
+            }
+        },
+
+        startDrag: function(e) {
+            if (e.which === 1) {
+                this.dragging = true;
+            }
+        },
+
+        endDrag: function(e) {
+            if (e.which === 1) {
+                this.dragging = false;
+                this.currentDrag = [];
+            }
+        },
+
+        handleSave: function() {
+            if (!this.model.has('name')) {
+                return alert("Enter letter");
+            }
+            fonts.add(this.model);
+            this.model.save();
+
+            router.navigate('fonts', {trigger: true});
+        },
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
         }
 
-        this.set('data', data);
-        this.trigger('change:data', this, data);
-    },
-});
-Fonts = Backbone.Collection.extend({
-    localStorage: new Backbone.LocalStorage("Fonts"),
-    model: Font
-});
+    });
 
-HomeView = Backbone.View.extend({
+    FontListView = Backbone.View.extend({
 
-    el: '.app',
+        className: '.font-list',
 
-    template: Handlebars.templates['home.hbs'],
+        template: Handlebars.templates['font-list.hbs'],
 
-    render: function() {
-        this.$el.html(this.template());
-    }
-});
+        initialize: function(opts) {
+            this.collection = opts.collection;
 
-FontEditView = Backbone.View.extend({
+            $('body').html(this.el);
+            this.render();
+        },
 
-    el: '.app',
+        render: function() {
+            this.$el.html(this.template(this.collection.toJSON()));
+            return this;
+        }
 
-    events: {
-        'click .cell': 'handleClick',
-        'change input[data-field]': 'handleChange',
-        'click button[data-action=save]': 'handleSave'
-    },
+    });
+    var fonts = new FontCollection();
 
-    template: Handlebars.templates['font-edit.hbs'],
+    Router = Backbone.Router.extend({
 
-    initialize: function(opts) {
-        this.model = opts.model;
-        console.log("model", this.model);
-        this.listenTo(this.model, 'change:data', this.render);
-    },
+        currentView: null,
 
-    handleChange: function(e) {
-        console.log("handleChange");
-        var $el = $(e.currentTarget),
-            field = $el.data('field');
+        routes: {
+            '': 'home',
+            'home':  'home',
+            'fonts': 'fonts',
+            'font/new': 'newFont',
+            'font/:id/edit': 'editFont',
+            'font/:id/delete': 'deleteFont',
+            'messages': 'messages',
+            'message/new': 'newMessage',
+            'message/:id/edit': 'editMessage',
+            'message/:id/delete': 'deleteMessage',
+        },
 
-        console.log("el", $el);
+        initialize: function() {
+            this.navigate('home');
+        },
 
-        this.model.set(field, $el.val());
-    },
+        home: function() {
+            this._loadView(new HomeView());
+        },
 
-    handleClick: function(e) {
-        var $el = $(e.currentTarget),
-            x = $el.data('x'),
-            y = $el.data('y');
+        fonts: function() {
+            fonts.fetch().then(_.bind(function() {
+                this._loadView(new FontListView({collection: fonts}));
+            },this));
+        },
 
-        this.model.toggleCell(x, y);
-    },
+        newFont: function() {
+            this._loadView(new FontEditView({model: new Font}));
+        },
 
-    handleSave: function() {
-        this.model.save();
-    },
-
-    render: function() {
-        console.log("render", this.model.toJSON());
-        this.$el.html(this.template(this.model.toJSON()));
-    }
-
-});
-
-FontListView = Backbone.View.extend({
-
-    el: '.app',
-
-    template: Handlebars.templates['font-list.hbs'],
-
-    initialize: function(opts) {
-        this.collection = opts.collection;
-    },
-
-    render: function() {
-        this.$el.html(this.template(this.collection.toJSON()));
-    }
-
-});
-
-Router = Backbone.Router.extend({
-
-    routes: {
-        '': 'home',
-        'home':  'home',
-        'fonts': 'fonts',
-        'font/new': 'newFont',
-        'font/:id/edit': 'editFont',
-        'font/:id/delete': 'deleteFont',
-        'messages': 'messages',
-        'message/new': 'newMessage',
-        'message/:id/edit': 'editMessage',
-        'message/:id/delete': 'deleteMessage',
-    },
-
-    initialize: function() {
-        this.navigate('home');
-        this.fonts = new Fonts();
-    },
-
-    home: function() {
-        var view = new HomeView();
-        view.render();
-    },
-
-    fonts: function() {
-        this.fonts.fetch();
-
-        var view = new FontListView({collection: this.fonts});
-        view.render();
-    },
-
-    newFont: function() {
-        var font = new Font();
-        this.fonts.add(font);
-        var view = new FontEditView({model: font});
-        view.render();
-    },
-
-    editFont: function(id) {
-        console.log("editFont", id);
-        this.fonts
-            .fetch()
-            .then(_.bind(function() {
-                var font = this.fonts.get(id);
-                font.fetch();
-                var view = new FontEditView({model: font});
-                view.render();
+        editFont: function(id) {
+            fonts.fetch().then(_.bind(function() {
+                this._loadView(new FontEditView({model: fonts.get(id)}));
             }, this));
-    }
+        },
 
+        deleteFont: function(id) {
+            var font = new Font({id: id});
+            font.delete();
+
+            router.navigate('fonts', {trigger: true});
+        },
+
+        _loadView: function(view, args) {
+            this.currentView && this.currentView.remove();
+            this.currentView = view;
+        }
+
+    });
+
+    router = new Router();
+
+    Backbone.history.start();
 });
-
-router = new Router();
-
-Backbone.history.start();
